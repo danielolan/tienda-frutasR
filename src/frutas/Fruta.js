42521172cb8068f67f3ca7b476b63f4e22f58e72
danielolan <60761999+danielolan@users.noreply.github.com>
2024-02-15 09:51:11 -0500

    frutas cambios temporales}

--- src/frutas/Fruta.js
+++ src/frutas/Fruta.js
@@ -1,14 +1,13 @@
-// Fruta.js
 import React from 'react';
 import './Fruta.css'; 
 
-function Fruta({ nombre, precio ,desscripcion}) {
+function Fruta({ nombre, precio, descripcion, imagen }) {
   return (
     <div className="fruta">
+      <img src={imagen} alt={`Imagen de ${nombre}`} />
       <h3>{nombre}</h3>
       <p>${precio}</p>
-      
-
+      <p>{descripcion}</p>
     </div>
   );
 }
